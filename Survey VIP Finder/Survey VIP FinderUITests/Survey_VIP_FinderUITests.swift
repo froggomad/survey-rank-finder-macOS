411d49f5375597c3af90da79cc8fbb2c91dfe8a3
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-10-10 15:13:13 -0700

    Add UI Testing Bundle

---
+++ Survey VIP Finder/Survey VIP FinderUITests/Survey_VIP_FinderUITests.swift
@@ -0,0 +1,58 @@
+//
+//  Survey_VIP_FinderUITests.swift
+//  Survey VIP FinderUITests
+//
+//  Created by Kenny on 10/10/20.
+//
+
+import XCTest
+@testable import Survey_VIP_Finder
+
+class Survey_VIP_FinderUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testCanInteractWithToolbar() {
+        let app = XCUIApplication()
+        app.launch()
+
+        let window = XCUIApplication().windows["Survey VIP Finder"]
+        let sortButton = window.toolbars.buttons["Sort"]
+        let openButton = window.toolbars.buttons["Open"]
+        let refreshButton = window.toolbars.buttons["Refresh"]
+
+        XCTAssertTrue(sortButton.isHittable)
+        XCTAssertTrue(openButton.isHittable)
+        XCTAssertTrue(refreshButton.isHittable)
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
