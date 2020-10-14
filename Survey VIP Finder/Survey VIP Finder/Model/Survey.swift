b5933e3b292b8353c05c727376472e0559eb575c
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-10-14 10:29:22 -0700

    assign parsed data to required properties

--- Survey VIP Finder/Survey VIP Finder/Model/Survey.swift
+++ Survey VIP Finder/Survey VIP Finder/Model/Survey.swift
@@ -12,10 +12,27 @@ import Foundation
 struct Survey: Codable {
     let filePath: String
     var cellData: [[String]] = []
+    var primaryLongFormColumns: [Int] = []
+    var secondaryLongFormColumns: [Int] = []
+    var emailColumn: Int?
+    var phoneColumn: Int?
 
     lazy var csvFileUrl: URL = URL(fileURLWithPath: filePath)
     // 10r, 27c
     mutating func read() {
+        /// Lower bounds of char count required for primary long form question column
+        /// - Note: if any column has an answer with this char count or higher
+        /// it will be considered a primary long-form question.
+        /// This logic could probably be improved to filter out edge cases...
+        /// maybe a certain percentage of answers hit this and the column
+        /// is considered a long-form question
+        let primaryLongFormCount = 100
+        // TODO: secondaryLongForm logic (count only works for long form overall)
+
+        /// Track the columnIndex as the loop iterates
+        /// - Note: Initialized at -1 due to unforseen edge case (1st column empty)
+        var columnIndex = -1
+
         do {
             let data = try String(contentsOf: csvFileUrl)
             // https://stackoverflow.com/questions/49206930/csv-parsing-swift-4
@@ -24,25 +41,54 @@ struct Survey: Codable {
             let regex = try! NSRegularExpression(pattern: pattern)
 
             var record: [String] = []
-            
-            for row in data.components(separatedBy: "\r") {
+            let dataArray = Array(data.components(separatedBy: "\r"))
+
+            for (index, row) in dataArray.enumerated() {
                 // apply rules to each block matched using the above regex
-                regex.enumerateMatches(in: row, options: .anchored, range: NSRange(0..<row.utf16.count)) {match, flags, stop in
+                regex.enumerateMatches(in: row, options: .anchored, range: NSRange(0..<row.utf16.count)) { match, _, stop in
                     guard let match = match else {
                         // TODO: Alert user
-                        print("unable to match the pattern for this csv file")
+                        print("unable to match the pattern for this row")
                         return
                     }
                     // inside quotes
                     if let quotedRange = Range(match.range(at: 1), in: row) {
                         let field = row[quotedRange].replacingOccurrences(of: "\"\"", with: "\"")
                         record.append(field)
+
+                        // TODO: Distinguish secondary long form questions
+                        // TODO: Make this a method
+                        if index != 0 && field.count >= primaryLongFormCount {
+                            if !primaryLongFormColumns.contains(columnIndex) {
+                                primaryLongFormColumns.append(columnIndex)
+                            }
+                        }
                     // tab?
                     } else if let range = Range(match.range(at: 2), in: row) {
                         let field = row[range].trimmingCharacters(in: .whitespaces)
                         record.append(field)
+                        // TODO: Distinguish secondary long form questions
+                        // TODO: Make this a method
+                        if index != 0 && field.count >= primaryLongFormCount {
+                            if !primaryLongFormColumns.contains(columnIndex) {
+                                primaryLongFormColumns.append(columnIndex)
+                            }
+                        }
                     }
-                    // ","
+                    // Track column
+                    if !cellData.isEmpty {
+                        let numColumns = cellData[0].count
+                        // Why can't I do a ternary expression here?
+                        // left side isn't mutable,expression evaluates to different type
+                        let indexOverCount = columnIndex == numColumns
+                        
+                        if indexOverCount {
+                            columnIndex = 0
+                        } else {
+                            columnIndex += 1
+                        }
+                    }
+
                     let separator = row[Range(match.range(at: 3), in: row)!]
                     switch separator {
                     case "":
@@ -62,8 +108,9 @@ struct Survey: Codable {
                     cellData[i].remove(at: 0)
                 }
             }
+            print(primaryLongFormColumns.sorted(by: {$0<$1}))
         } catch {
-            print(error)
+            print("Error decoding file: \(error)")
         }
     }
 
