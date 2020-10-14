c254af5fe67909c94956305c5bb798a53457d92c
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-10-14 16:54:19 -0700

    increase header score to unreachable number to prevent it from being sorted

--- Survey VIP Finder/Survey VIP Finder/Model/Row.swift
+++ Survey VIP Finder/Survey VIP Finder/Model/Row.swift
@@ -8,11 +8,16 @@
 import Foundation
 
 struct Row: Codable {
+    var id: Int
     var fields: [Field]
     var score: Int {
-        let longFormFields = fields.filter { $0.isLongForm }
-        let scores = longFormFields.map { $0.score }
-        // add scores together
-        return scores.reduce(0, +)
+        if id != 0 {
+            let longFormFields = fields.filter { $0.isLongForm }
+            let scores = longFormFields.map { $0.score }
+            // add scores together
+            return scores.reduce(0, +)
+        }
+        // prevent row from being sorted
+        return 999_999_999
     }
 }
