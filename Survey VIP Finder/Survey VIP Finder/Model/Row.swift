1b8ba62eea9fa2e3046fc46af1bb7b72e5ce08f0
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-10-15 20:17:25 -0700

    add required fields to data model

--- Survey VIP Finder/Survey VIP Finder/Model/Row.swift
+++ Survey VIP Finder/Survey VIP Finder/Model/Row.swift
@@ -8,8 +8,13 @@
 import Foundation
 
 struct Row: Codable {
+    // MARK: - Properties -
     var id: Int
+    var emailColumn: Int?
+    var phoneColumn: Int?
+
     var fields: [Field]
+
     var score: Int {
         if id != 0 {
             let longFormFields = fields.filter { $0.isLongForm }
@@ -19,5 +24,52 @@ struct Row: Codable {
         }
         // prevent row from being sorted
         return 999_999_999
+    }
+
+    // MARK: - Methods -
+    mutating func addFields() {
+        fields.append(addSMIQField())
+        fields.append(addEmailandPhoneField())
+        fields.append(addScoreField())
+    }
+
+    private var SMIQScore: Int? {
+        var longFields = fields.filter { $0.isLongForm }
+        if !longFields.isEmpty {
+            longFields.sort { $0.score > $1.score }
+
+            let highScore = longFields[0].score
+
+            return highScore
+        }
+
+        return nil
+    }
+
+    private func addSMIQField() -> Field {
+        let highScore = SMIQScore ?? 0
+
+        let text = String(highScore)
+        return Field(text: text)
+    }
+
+    private func addEmailandPhoneField() -> Field {
+        var emailValue: String = ""
+        var phoneValue: String = ""
+
+        if let emailColumn = emailColumn {
+            emailValue = fields[emailColumn].text
+        }
+
+        if let phoneColumn = phoneColumn {
+            phoneValue = fields[phoneColumn].text
+        }
+
+        return Field(text: "\(emailValue) \n \(phoneValue)")
+    }
+
+    private func addScoreField() -> Field {
+        let text = String(score)
+        return Field(text: text)
     }
 }
